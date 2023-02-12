fe74d45d4a9cf90ec594b4203dfd8c85dbf64826
Dair Sheishenaliev <dokadair@gmail.com>
2023-02-12 08:11:33 +0600

    Change re_template ForeignKey to ManyToMany

--- apps/dlp/models.py
+++ apps/dlp/models.py
@@ -4,16 +4,19 @@ from django.db import models
 class ReTemplate(models.Model):
     pattern = models.TextField(verbose_name='Regex Pattern')
 
+    @property
+    def rpattern(self):
+        return fr'{self.pattern}'
+
     def __str__(self):
         return f'{self.id} - {self.pattern}'
 
 
 class BaseMessage(models.Model):
-    re_template = models.ForeignKey(
+    re_templates = models.ManyToManyField(
         ReTemplate,
-        on_delete=models.CASCADE,
         related_name='messages',
-        verbose_name='Re Template',
+        verbose_name='Re Templates',
     )
 
     text = models.TextField(null=True, blank=True, verbose_name='Text')
